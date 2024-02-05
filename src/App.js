bf45dd98262d9b14dc081c60ecffca00ab98d74e
Rasyahodhe <rasyahodhe450@gmail.com>
2024-02-05 20:26:10 +0700

    menambahkan sedikit image skill dan menambahkan fungsi map pada skill.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const NavBar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon btn_size_cs"></span>
         </button>
         <div
           class="collapse navbar-collapse justify-content-end"
--- src/component/Skill.js
+++ src/component/Skill.js
@@ -2,30 +2,32 @@ import "../boostrap/boostrap.css";
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "./css/Skill.css";
 
+var Skillku = [
+  { namaSkill: "Javascript", sr: "" },
+  { namaSkill: "HTML", sr: "" },
+  { namaSkill: "CSS", sr: "" },
+  { namaSkill: "React Js", sr: "" },
+  { namaSkill: "Boostrap", sr: "" },
+];
+
 const NameSkill = (props) => {
   return (
     <li>
-      <h2>{props.skillname}</h2>
+      <div className="skl_li_content">
+        <img src={props.namegambar}></img>
+        <h2>{props.skillname}</h2>
+      </div>
     </li>
   );
 };
 
 const Skill = () => {
-  var Skillku = [
-    "JavaScript",
-    "HTML",
-    "CSS",
-    "React Js",
-    "Bostrap",
-    "bulma",
-    "materealize",
-  ];
   return (
     <>
-      <h1>MY SKILL</h1>
-      <ul>
+      <h1 className="skl_head">My Skill</h1>
+      <ul className="skl_container_element">
         {Skillku.map((skills) => (
-          <NameSkill skillname={skills} />
+          <NameSkill skillname={skills.namaSkill} namegambar={skills.sr} />
         ))}
       </ul>
     </>
